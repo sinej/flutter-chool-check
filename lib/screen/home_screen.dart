54898caae4b79317e92e61c267d5bee5076fc17c
shin <sinej1020@gmail.com>
2024-05-20 16:11:13 +0900

    feat: 출첵 로직 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -18,7 +19,33 @@ class _HomeScreenState extends State<HomeScreen> {
     zoom: 15,
   );
 
+  bool choolCheckDone = false;
+  bool canChoolCheck = false;
+
+  final double okDistance = 100;
+
   late final GoogleMapController controller;
+
+  @override
+  initState() {
+    super.initState();
+
+    Geolocator.getPositionStream().listen((event) {
+      final start = LatLng(37.5214, 126.9246);
+
+      final end = LatLng(event.latitude, event.longitude);
+
+      final distance = Geolocator.distanceBetween(
+          start.latitude, start.longitude, end.latitude, end.longitude);
+      setState(() {
+        if (distance > okDistance) {
+          canChoolCheck = false;
+        } else {
+          canChoolCheck = true;
+        }
+      });
+    });
+  }
 
   checkPermission() async {
     final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
@@ -71,30 +98,64 @@ class _HomeScreenState extends State<HomeScreen> {
           return Column(
             children: [
               Expanded(
-                  child: GoogleMap(
-                    initialCameraPosition: initialPosition,
-                    mapType: MapType.normal,
-                    myLocationEnabled: true,
-                    myLocationButtonEnabled: false,
-                    zoomControlsEnabled: false,
-                    onMapCreated: (GoogleMapController controller){
-                      this.controller = controller;
-                    },
-                    markers: {
-                      Marker(
-                        markerId: MarkerId('location-marker'),
-                        position: LatLng(
-                          37.5214,
-                          126.9246
-                        ),
-                      )
-                    },
-              ))
+                flex: 2,
+                child: _GoogleMap(
+                  initialCameraLocation: initialPosition,
+                  onMapCreated: (GoogleMapController controller) {
+                    this.controller = controller;
+                  },
+                  canChoolCheck: canChoolCheck,
+                  radius: okDistance,
+                ),
+              ),
+              Expanded(
+                child: _BottomChoolCheckButton(
+                  choolCheckDone: choolCheckDone,
+                  canChoolCheck: canChoolCheck,
+                  choolCheckPressed: choolCheckPressed,
+                ),
+              )
             ],
           );
         },
       ),
     );
+  }
+  choolCheckPressed() async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text('출근하기'),
+          content: Text('출근을 하시겠습니까?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.red,
+                ),
+                child: Text('취소')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black,
+                ),
+                child: Text('출근하기')),
+          ],
+        );
+      },
+    );
+
+    if (result) {
+      setState(() {
+        choolCheckDone = result;
+        print(result);
+      });
+    }
   }
 
   myLocationPressed() async {
@@ -102,11 +163,88 @@ class _HomeScreenState extends State<HomeScreen> {
 
     controller.animateCamera(
       CameraUpdate.newLatLng(
-        LatLng(
-          location.latitude,
-          location.longitude
-        ),
+        LatLng(location.latitude, location.longitude),
       ),
+    );
+  }
+}
+
+class _GoogleMap extends StatelessWidget {
+  final CameraPosition initialCameraLocation;
+  final MapCreatedCallback onMapCreated;
+  final bool canChoolCheck;
+  final double radius;
+
+  const _GoogleMap({
+    required this.initialCameraLocation,
+    required this.onMapCreated,
+    required this.canChoolCheck,
+    required this.radius,
+    super.key
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: initialCameraLocation,
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      zoomControlsEnabled: false,
+      onMapCreated: onMapCreated,
+      markers: {
+        Marker(
+          markerId: MarkerId('location-marker'),
+          position: LatLng(37.5214, 126.9246),
+        )
+      },
+      circles: {
+        Circle(
+          circleId: CircleId('indistance'),
+          center: LatLng(37.5214, 126.9246),
+          radius: radius,
+          fillColor: canChoolCheck ? Colors.blue.withOpacity(.3) : Colors.red.withOpacity(.3),
+          strokeColor: canChoolCheck ? Colors.blue : Colors.red,
+          strokeWidth: 1,
+        ),
+      },
+    );
+  }
+}
+
+class _BottomChoolCheckButton extends StatelessWidget {
+  final bool choolCheckDone;
+  final bool canChoolCheck;
+  final VoidCallback choolCheckPressed;
+
+
+  const _BottomChoolCheckButton({
+    required this.choolCheckDone,
+    required this.canChoolCheck,
+    required this.choolCheckPressed,
+    super.key
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          choolCheckDone ? Icons.check : Icons.timelapse_outlined,
+          color: choolCheckDone ? Colors.green : Colors.blue,
+        ),
+        SizedBox(height: 16.0),
+        if (!choolCheckDone && canChoolCheck)
+          OutlinedButton(
+            onPressed: choolCheckPressed,
+            style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.blue),
+            child: Text('출근하기'),
+          )
+      ],
     );
   }
 }
