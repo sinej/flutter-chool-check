55458c4761041a824cad9166e471ed33f0d68518
shin <sinej1020@gmail.com>
2024-05-20 14:35:10 +0900

    feat: google map location 추가

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 - Flutter
 - Google Maps (google_maps_flutter ^2.6.1)
+- geolocator ^12.0.0
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final CameraPosition initialPosition = CameraPosition(
+    target: LatLng(
+      37.5214,
+      126.9246,
+    ),
+    zoom: 15,
+  );
+
+  late final GoogleMapController controller;
+
+  checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      throw Exception('위치 가능을 활성화 해주세요.');
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      checkedPermission = await Geolocator.requestPermission();
+    }
+
+    if (checkedPermission != LocationPermission.always &&
+        checkedPermission != LocationPermission.whileInUse) {
+      throw Exception('위치 권한을 허가 해주세요.');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          '오늘도 출근',
+          style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: myLocationPressed,
+            icon: Icon(Icons.my_location),
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: checkPermission(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          return Column(
+            children: [
+              Expanded(
+                  child: GoogleMap(
+                    initialCameraPosition: initialPosition,
+                    mapType: MapType.normal,
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    zoomControlsEnabled: false,
+                    onMapCreated: (GoogleMapController controller){
+                      this.controller = controller;
+                    },
+                    markers: {
+                      Marker(
+                        markerId: MarkerId('location-marker'),
+                        position: LatLng(
+                          37.5214,
+                          126.9246
+                        ),
+                      )
+                    },
+              ))
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  myLocationPressed() async {
+    final location = await Geolocator.getCurrentPosition();
+
+    controller.animateCamera(
+      CameraUpdate.newLatLng(
+        LatLng(
+          location.latitude,
+          location.longitude
+        ),
+      ),
+    );
   }
 }
